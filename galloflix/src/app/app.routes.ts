e7d2d1f3998eb73c67484f3c1a9893d02678d31c
IttaRodrigues <itamararodriguesdeoliveira@gmail.com>
2025-05-14 20:34:12 -0300

    Criado projeto, componentes

---
+++ galloflix/src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { SearchComponent } from './pages/search/search.component';
+import { DetailsComponent } from './pages/details/details.component';
+
+export const routes: Routes = [   
+        { path: '', redirectTo: 'home', pathMatch: 'full'},
+        { path: 'home', component: HomeComponent},
+        { path: 'search', component: SearchComponent},
+        { path: 'details', component: DetailsComponent}
+    
+];
