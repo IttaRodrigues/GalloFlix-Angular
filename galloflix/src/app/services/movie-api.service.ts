f921a6e5ac5936dd64d77133d3aa738f5af8652f
IttaRodrigues <itamararodriguesdeoliveira@gmail.com>
2025-05-23 21:05:44 -0300

    Inclusão da página de dados

--- galloflix/src/app/services/movie-api.service.ts
+++ galloflix/src/app/services/movie-api.service.ts
@@ -1,13 +1,26 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieApiService {
-
+  
   constructor(private http: HttpClient) { }
 
-  
+  baseUrl = 'https://api.themoviedb.org/3';
+  options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMjkyNTJjOWFmMDUxNjIyOWVmMDhjMDk4MzIyNTcwNSIsIm5iZiI6MTc0NjgzODkyNi4zNzY5OTk5LCJzdWIiOiI2ODFlYTU4ZTEwMjg1MjU2ODUyZDBlYjgiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.cjxiArhjCZwl-M4U3ysjSNvJFGlrjH6Py5m_sUE4_cs'
+  }
+};
+
+// Banner de Midias da Semana
+bannerApiData(): Observable<any> {
+  return this.http.get(`${this.baseUrl}/trending/all/week?language=pt-br`, this.options);
+}
 
 }
